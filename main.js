3c994dcdd1df30fb37ec6a63c357bdb7baa26535
AdrikLarreategui <adrian15186@hotmail.com>
2023-10-27 20:09:46 +0200

    commit

---
+++ main.js
@@ -0,0 +1,25 @@
+//Funciones flecha.
+
+// function saludar() {
+//     return "Hola";
+//   }
+  //Convertir la funcion anterior en flecha
+
+const saludar = () => "Hola"
+console.log(saludar())
+
+// Convierte la siguiente función en una función flecha en línea:
+// function division(a,b) {
+//   return a / b;
+// }
+
+const division = (a,b) => a / b;
+console.log(division(6,2))
+
+// Convierte la siguiente función en una función flecha:
+// function miNombre(nombre) {
+//   return `Mi nombre es ${nombre}`;
+// }
+
+const miNombre = (nombre) => `Mi nombre es ${nombre}`
+console.log(miNombre("Adrián"))
